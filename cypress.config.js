76f83a8f07de5eef5eabd7c259a9db6c61a3eed0
hyxt <hyxt@vng.com.vn>
2023-08-28 19:47:30 +0700

    implement HTML report

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,13 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'https://id.zalo.me/account?continue=https%3A%2F%2Fchat.zalo.me%2F',
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
       allureWriter(on, config);
       return config;
     },
